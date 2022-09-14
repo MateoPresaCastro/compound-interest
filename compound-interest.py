58c08279738c3f9fb486f310db84196aef8357fe
Mateo Presa Castro <mateopresacastro@gmail.com>
2022-09-14 10:27:13 +0200

    Initial commit

---
+++ compound-interest.py
@@ -0,0 +1,28 @@
+import pyinputplus as pyip
+
+print("\n*** Index fund yearly compound interest calculator ***\n")
+
+print("Initial investment:",end=" ")
+initialInvestment = pyip.inputNum()
+
+print("Monthly Contribution:",end=" ")
+monthlyContribution = pyip.inputNum()
+
+monthlyContribution *= 12
+n = 1
+
+print("Length of Time in Years:",end=" ")
+lengthOfTime = pyip.inputNum()
+
+print("Estimated Interest Rate:",end=" ")
+interestRate = pyip.inputNum()
+
+
+yearlyCompound = initialInvestment*((1+((interestRate/100)/n))**(n*lengthOfTime))
+recurringPayment = monthlyContribution*(((1+(interestRate/100))**(n*lengthOfTime)-1)/(interestRate/100))
+finalAmount = yearlyCompound + recurringPayment
+
+
+print(f"Final amount: ${round(finalAmount,2)}");
+
+
